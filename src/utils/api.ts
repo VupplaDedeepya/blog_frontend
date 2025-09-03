e2c62c38510bef37fc300fb2ebdc7e509dc5eac6
VupplaDedeepya <vuppaladedeepya@gmail.com>
2025-09-03 12:16:15 +0530

    added .env file.

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3005/api", // Replace with your NestJS API base URL
+  baseURL: process.env.NEXT_API_BASE_URL, // Replace with your NestJS API base URL
 });
 
 export default api;
